6dc9330e4f53689140431949590ddbd665873c6b
maximiliancsuk <maximilian.csuk@max-bytes.com>
2025-02-14 09:33:33 +0100

    bugfix

--- build.ps1
+++ build.ps1
@@ -7,13 +7,14 @@ if (-Not (Test-Path('.\Godot_v3.6-stable_mono_win64'))) {
 
 # this should be the correct target folder for mods in most cases
 $targetDir = "$($env:APPDATA)\Godot\app_userdata\Card Stuff\mods"
+New-Item -ItemType Directory -Force -Path $targetDir
 
 # build godot project
 Godot_v3.6-stable_mono_win64\Godot_v3.6-stable_mono_win64.exe --no-window --path . --export-pack "Build" "$($targetDir)\ExampleMod.pck" | Out-Default
 
 # build c# project
-msbuild.exe ExampleMod.csproj /t:Restore /p:Configuration=ExportDebug | Out-Default
-msbuild.exe ExampleMod.csproj /t:Build /p:Configuration=ExportDebug | Out-Default
+dotnet build ExampleMod.csproj /t:Restore /p:Configuration=ExportDebug | Out-Default
+dotnet build ExampleMod.csproj /t:Build /p:Configuration=ExportDebug | Out-Default
 
 # copy dll to target folder
 Copy-Item ".mono\temp\bin\ExportDebug\ExampleMod.dll" -Destination $targetDir
